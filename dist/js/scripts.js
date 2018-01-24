f557e96b5c5a2d46ba859d3c3f0fe3e1ca2ef001
bekirova <mail@bekirova.ru>
2018-01-24 17:20:37 +0700

    added page contact

---
+++ dist/js/scripts.js
@@ -0,0 +1,33 @@
+// фиксированное меню при скроле страницы до него
+$(document).ready(function(){
+    var HeaderTop = $('.menu').offset().top;
+    var pad = $('.menu').css('height');
+
+    $(window).scroll(function(){
+        if( $(window).scrollTop() > HeaderTop ) {
+            $('.menu').css({position: 'fixed', top: '0px', left: '0px', right: '0px'});
+            $('.base').css('padding-top', pad);
+        } else {
+            $('.menu').css({position: 'relative'});
+            $('.base').css('padding-top', '0px');
+        }
+    });
+});
+
+// открытие меню при клике
+$(function () {
+    $('.icon').on('click', function () {
+        $('.menu_container').slideToggle(400, function () {
+            if ($(this).css('display') === 'none') {
+                $(this).removeAttr('style');
+            }
+        });
+    });
+});
+
+$(window).load(function() {
+    $('.flexslider').flexslider({
+        animation: "slide",
+        slideshowSpeed: 3000// время переключения слайдов
+    });
+});
